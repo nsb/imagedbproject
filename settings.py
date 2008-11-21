aeed30f8cc0c5a0730f9726a51a7aefcb1a997d4
Niels Sandholt Busch <niels.busch@gmail.com>
2008-11-22 00:20:56 +0100

    remove contrib.sites

--- settings.py
+++ settings.py
@@ -89,7 +89,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.admin',
     'tagging',
     'photologue',
