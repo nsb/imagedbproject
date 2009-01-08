076df59da6596196d024e55cdc65793646a9e018
Niels Sandholt Busch <niels.busch@gmail.com>
2009-01-08 22:53:28 +0100

    use hashlib module instead of md5

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import os
-import md5
+import hashlib
 from django.conf import settings
 
 def get_image_path(instance, filename):
     return os.path.join(
         'photos',
-        md5.new(''.join(
+        hashlib.md5(''.join(
             (str(instance.date_added.date()),
              settings.SECRET_KEY))).hexdigest().lower(), filename)
