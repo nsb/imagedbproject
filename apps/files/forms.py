bd7f69f7968017ec9ad2eacee49f50959fa80883
Niels Sandholt Busch <niels@busch@gmail.com>
2009-03-10 15:36:22 +0100

    fix form

--- apps/files/forms.py
+++ apps/files/forms.py
@@ -4,16 +4,18 @@ from django.forms import ModelForm
 from django import forms
 
 from models import Image
-from categories.models import Location, Installation, People, HSE, Event, Graphics, Communications
+from categories.models import Location, Field, Installation, People, HSE, Event, Graphics, Communications, Archive
 
 class ImageFilterForm(ModelForm):
     locations = forms.ChoiceField(label='Locations', required=False)
+    fields = forms.ChoiceField(label='Fields', required=False)
     installations = forms.ChoiceField(label='Installations & Vessels', required=False)
     people = forms.ChoiceField(label='People', required=False)
     hse = forms.ChoiceField(label='HSE', required=False)
     events = forms.ChoiceField(label='Events', required=False)
     graphics = forms.ChoiceField(label='Graphics', required=False)
     communications = forms.ChoiceField(label='Communications', required=False)
+    archives = forms.ChoiceField(label='Archives', required=False)
 
     def __init__(self, *args, **kwargs):
         super(ImageFilterForm, self).__init__(*args, **kwargs)
@@ -22,13 +24,15 @@ class ImageFilterForm(ModelForm):
             [(item.name, '%s (%d)' % (item.name, item.image_set.count())) for item in x]
 
         self.fields['locations'].choices = _choices(Location.objects.all())
+        self.fields['fields'].choices = _choices(Field.objects.all())
         self.fields['installations'].choices = _choices(Installation.objects.all())
         self.fields['people'].choices = _choices(People.objects.all())
         self.fields['hse'].choices = _choices(HSE.objects.all())
         self.fields['events'].choices = _choices(Event.objects.all())
         self.fields['graphics'].choices = _choices(Graphics.objects.all())
         self.fields['communications'].choices = _choices(Communications.objects.all())
+        self.fields['archives'].choices = _choices(Archive.objects.all())
 
     class Meta:
         model = Image
-        fields = ('locations', 'installations', 'people', 'hse', 'events', 'graphics', 'communications',)
+        fields = ('locations', 'fields', 'installations', 'people', 'hse', 'events', 'graphics', 'communications', 'archives',)
