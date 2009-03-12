6a6f3d181016e1f024a66f8378202fc5c04d845d
Niels Sandholt Busch <niels@busch@gmail.com>
2009-03-12 16:29:49 +0100

    Revert "more select width tests"
    This reverts commit 7bc3e13846ebe61bfaec40dc739200b55aeb3531.

---
+++ static/js/iefix.js
@@ -0,0 +1,16 @@
+
+  $(function() {
+
+    $("select")
+
+      .mouseover(function(){
+          $(this)
+              .data("origWidth", $(this).css("width"))
+              .css("width", "auto");
+      })
+
+      .mouseout(function(){
+          $(this).css("width", $(this).data("origWidth"));
+      });
+
+  });
