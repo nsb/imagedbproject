b83d5c3fe896b3df0bc393671f71a036cc4b9af2
Niels Sandholt Busch <niels@busch@gmail.com>
2009-05-05 12:08:01 +0200

    add image mouse over

---
+++ static/js/custom.js
@@ -0,0 +1,24 @@
+jQuery.preloadImages = function()
+{
+  for(var i = 0; i<arguments.length; i++)
+  {
+    jQuery("<img>").attr("src", arguments[i]);
+  }
+}
+
+function toggleImage(buttonElm, imgover, imgout) {
+  buttonElm.mouseover(function(e) {
+    $(this).attr("src", imgover); 
+  }).mouseout(function(e) {
+    $(this).attr("src", imgout);
+  });
+} 
+
+$(document).ready(function(){
+
+  $.preloadImages("/static/img/button_09.gif");
+
+  toggleImage($('#searchbutton'), "/static/img/button_09.gif", "/static/img/button_07.gif"); 
+  toggleImage($('#resetlink'), "/static/img/button_05.gif", "/static/img/button_03.gif"); 
+
+});
