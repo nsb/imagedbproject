d04ed1b4f1afebaa66006c1435cd37abd3034ee4
Niels Sandholt Busch <niels@busch@gmail.com>
2009-05-12 16:13:39 +0200

    add cmyk and pantone buttons

--- static/js/custom.js
+++ static/js/custom.js
@@ -25,7 +25,9 @@ $(document).ready(function(){
                   "/static/img/Small2.png",
                   "/static/img/Medium2.png",
                   "/static/img/Large2.png",
-                  "/static/img/original_size2.png");
+                  "/static/img/original_size2.png",
+                  "/static/img/cmyk2.png",
+                  "/static/img/pantone2.png");
 
   toggleImage($('#searchbutton'), "/static/img/Search2.png", "/static/img/Search.png"); 
   toggleImage($('#resetlink'), "/static/img/clear2.png", "/static/img/clear.png");
@@ -35,8 +37,9 @@ $(document).ready(function(){
   toggleImage($('#download-medium'), "/static/img/Medium2.png", "/static/img/Medium.png"); 
   toggleImage($('#download-large'), "/static/img/Large2.png", "/static/img/Large.png"); 
   toggleImage($('#download-original'), "/static/img/original_size2.png", "/static/img/original_size.png"); 
+  toggleImage($('#download-cmyk'), "/static/img/cmyk2.png", "/static/img/cmyk.png"); 
+  toggleImage($('#download-pantone'), "/static/img/pantone2.png", "/static/img/pantone.png");
   toggleImage($('.logos #images-section'), "/static/img/Images2.png", "/static/img/Images.png"); 
   toggleImage($('.images #logos-section'), "/static/img/logos2.png", "/static/img/logos.png"); 
-
 
 });
