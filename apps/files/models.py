bb3cfdff592109ff7e922482020fc36290a98645
Niels Sandholt Busch <niels@busch@gmail.com>
2009-05-19 13:17:00 +0200

    change logo width and height

--- apps/files/models.py
+++ apps/files/models.py
@@ -275,7 +275,7 @@ class EPS(models.Model):
 
     def resize_thumbnail(self, im):
         cur_width, cur_height = im.size
-        new_width, new_height = (400, 400)
+        new_width, new_height = (275, 275)
         if not new_width == 0 and not new_height == 0:
             ratio = min(float(new_width)/cur_width,
                         float(new_height)/cur_height)
