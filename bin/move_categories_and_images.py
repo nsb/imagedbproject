05888d32766139645491de0b4adb7ae996d50af2
Niels Sandholt Busch <niels@busch@gmail.com>
2009-05-19 14:44:35 +0200

    add script to move categories

--- bin/move_categories_and_images.py
+++ bin/move_categories_and_images.py
@@ -4,6 +4,7 @@ import sys, os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../apps')
+sys.path.append("/home/ciboe/lib")
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from categories.models import Event, Communications
