9dd58ad7560444d7c9731b1ab1e8d06188f3a0bd
Niels Sandholt Busch <niels@busch@gmail.com>
2009-06-04 11:43:15 +0200

    add new buttons

--- static/js/custom.js
+++ static/js/custom.js
@@ -42,8 +42,8 @@ $(document).ready(function(){
   toggleImage($('#download-medium'), "/static/img/Medium2.png", "/static/img/Medium.png"); 
   toggleImage($('#download-large'), "/static/img/Large2.png", "/static/img/Large.png"); 
   toggleImage($('#download-original'), "/static/img/original_size2.png", "/static/img/original_size.png"); 
-  toggleImage($('#download-cmyk'), "/static/img/cmyk2.png", "/static/img/cmyk1.png"); 
-  toggleImage($('#download-pantone'), "/static/img/pantone2.png", "/static/img/pantone1.png");
+  toggleImage($('#download-cmyk'), "/static/img/cmyk2.png", "/static/img/cmyk.png"); 
+  toggleImage($('#download-pantone'), "/static/img/pantone2.png", "/static/img/pantone.png");
   toggleImage($('.logos #images-section'), "/static/img/Images2.png", "/static/img/Images.png"); 
   toggleImage($('.images #logos-section'), "/static/img/logos2.png", "/static/img/logos.png"); 
 
