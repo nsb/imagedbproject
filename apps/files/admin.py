3c1ce43eb7c89cee1892222a57638232c1187834
Niels Sandholt Busch <niels@busch@gmail.com>
2009-06-11 14:06:55 +0200

    change admin list per page

--- apps/files/admin.py
+++ apps/files/admin.py
@@ -25,7 +25,7 @@ def bulk_caption(modeladmin, request, queryset):
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_added', 'is_public', 'view_count', 'admin_thumbnail')
     list_filter = ['date_added', 'is_public', 'locations', 'installations', 'people', 'hse', 'graphics', 'communications', 'years', 'archives']
-    list_per_page = 200
+    list_per_page = 20
     save_on_top = True
     actions = [bulk_caption, 'set_1985', 'set_1986', 'set_1987', 'set_1988', 'set_1989', 'set_1990', 'set_1991', 'set_1992', 'set_1993', 'set_1994', 'set_1995', 'set_1996', 'set_1997', 'set_1998', 'set_1999', 'set_2000', 'set_2001', 'set_2002', 'set_2003', 'set_2004', 'set_2005', 'set_2006', 'set_2007', 'set_2008', 'set_2009',]
     fieldsets = (
