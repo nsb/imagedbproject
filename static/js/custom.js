c587370394beee3a8c19d1df48ef6a32e9ffba7a
Niels Sandholt Busch <niels.busch@gmail.com>
2009-06-30 13:24:11 +0200

    revert johans shopping cart

--- static/js/custom.js
+++ static/js/custom.js
@@ -17,7 +17,6 @@ function toggleImage(buttonElm, imgover, imgout) {
 $(document).ready(function(){
 
   // $('body').supersleight();
-
   $("#rightbox").tabs({ selected: 3 });
   // try to hide tab items, except default
 
