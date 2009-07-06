5f353ccef88aa2c468caaf6645285caafc8eb4e6
Niels Sandholt Busch <niels.busch@gmail.com>
2009-07-06 10:03:06 +0200

    more admin text changes

--- apps/files/admin.py
+++ apps/files/admin.py
@@ -339,12 +339,12 @@ class EPSAdmin(admin.ModelAdmin):
 
 class UserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff',)
-    list_filter = ['is_staff', 'is_superuser',] 
+    list_filter = ['is_staff',]
     fieldsets = (
         (None, {
             'fields': ('username', 'password',)
         }),
-        ('Personal info', {
+        ('Personal info (optional)', {
             'fields': ('first_name', 'last_name', 'email',)
         }),
         ('Permissions', {
