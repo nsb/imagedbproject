75aba9ddf32d6f830eff4d89e41ccb63eb657e75
Gora Mohanty <gora@srijan.in>
2009-07-06 11:54:00 +0530

    Started integrating unit tests

--- middleware/tests/test_cases/test_ip_addr_spec.py
+++ middleware/tests/test_cases/test_ip_addr_spec.py
@@ -1,16 +1,38 @@
-import unittest
-# Hack to get to ip_addr_filter.pyx
+from os import system
+# Hack to get to middleware.py
 import sys
-sys.path = [ "../" ] + sys.path
-from ip_addr_filter import IPAddressMiddleWare
+sys.path = [ "../../" ] + sys.path
+import unittest
+from middleware import IPAddressMiddleware
+from netaddr import AddrFormatError
+
+LOCAL_SETTINGS_FILE = '../../settings_local.py'
+LOCAL_SETTINGS_FILE_BAK = '../../settings_local.py.bak'
 
 class TestIPAddressSpec(unittest.TestCase):
 
-    def test01_simple(self):
-        """Simple test
+    def setUp(self):
+        system( 'cp ' + LOCAL_SETTINGS_FILE + ' ' + LOCAL_SETTINGS_FILE_BAK )
+
+    def tearDown(self):
+        system( 'mv ' + LOCAL_SETTINGS_FILE_BAK + ' ' + LOCAL_SETTINGS_FILE )
+
+    def test01_invalid_ip(self):
+        """Test for invalid IPs. An AddFormatError exception should be thrown
         """
-        self.assertEqual( 1, 1 )
-        #self.assertRaises( );
+#         INVALID_IP = '"192.168.1.256"'
+#         system( r"sed 's/\(^SRJ_IP_REG = \)\(.*\)/\1\[" + \
+#                     str( INVALID_IP ) + "]/' " + LOCAL_SETTINGS_FILE + \
+#                     " > tmp.$$ && cp tmp.$$ " + LOCAL_SETTINGS_FILE )
+#         self.assertRaises( Exception, IPAddressMiddleware ) 
+
+    def test02_valid_ip(self):
+        """Test for valid IPs.
+        """
+        try:
+            IPAddressMiddleware()
+        except Exception, e:
+            self.fail( 'Unexpected exception. Error message is:\n' + str(e) )
 
 def suite():
     s = unittest.TestSuite()
