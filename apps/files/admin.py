e3106b5cf5bb312e4121fc8f0485f26cc497aef5
Johan Bichel Lindegaard <mr.bichel@gmail.com>
2009-07-07 15:54:23 +0200

    Revert "more admin text changes"
    This reverts commit 5f353ccef88aa2c468caaf6645285caafc8eb4e6.
    
    Signed-off-by: Johan Bichel Lindegaard <mr.bichel@gmail.com>

--- apps/files/admin.py
+++ apps/files/admin.py
@@ -339,12 +339,12 @@ class EPSAdmin(admin.ModelAdmin):
 
 class UserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff',)
-    list_filter = ['is_staff',]
+    list_filter = ['is_staff', 'is_superuser',] 
     fieldsets = (
         (None, {
             'fields': ('username', 'password',)
         }),
-        ('Personal info (optional)', {
+        ('Personal info', {
             'fields': ('first_name', 'last_name', 'email',)
         }),
         ('Permissions', {
